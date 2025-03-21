6a5dee1593f64e20c4959b8de052482932539975
feh <felix.haak@imp-ag.de>
2025-03-21 12:00:46 +0100

    Update base path in Vite config
    Changed the `base` path from a relative path (`./floppypoct/`) to an absolute one (`/floppypoct/`). This ensures consistent behavior across different deployment environments.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './floppypoct/',
+  base: '/floppypoct/',
   resolve: {
     extensions: ['.tsx']
   }
